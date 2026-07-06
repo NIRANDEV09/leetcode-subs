352b2472ffc92396e556ae4ad87225d8b764e03a
Nirandev C M <nirandevcm.cs@gmail.com>
2026-07-06 17:38:31 +0530

    subs 9 commit

---
+++ c_basic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a , int b , int c , int d){
+    int max = 0 ;
+    int arr[5]={a,b,c,d};
+    for (int i =0;i<5;i++){
+        if (arr[i]>max)
+            max=arr[i];
+        else{
+            continue;
+        }
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
